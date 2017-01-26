fc35e491ea185dce030321e9684f3071e33bf9ef
manish181192 <manishvidyasagar@gmail.com>
2017-01-27 01:33:45 +0530

    Basic neural network - aplha_version

--- NN_handle.py
+++ NN_handle.py
@@ -1,12 +1,20 @@
 from NeuralNetwork import NeuralNetwork
+import numpy as np
 
 no_of_features = 3
 train_data = [[0,1,2],[0,1,1],[1,0,1],[2,1,0]]
-labels = [12,9,8,16]
+labels = [1,1,0,1]
 hidden_layer_sizes = [10]
 output_classes = 1
 
+feed_dict = {}
+
+for i in enumerate(labels):
+    feed_dict['label'] = np.asarray(labels)
+    feed_dict['input'] = np.asarray(train_data)
+
 nn = NeuralNetwork(no_of_input_features= no_of_features, hidden_layer_sizes=hidden_layer_sizes, output_classes=output_classes)
-nn.set_datapoint(train_data[0], label= labels[0])
-print(nn.forward_feed_datapoint())
-nn.back_propagation_datapoint()
+# nn.set_datapoint(train_data[0], label= labels[0])
+# print(nn.forward_feed_datapoint())
+# nn.back_propagation_datapoint()
+nn.train(no_of_epochs= 100, feed_dict = feed_dict)
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,20 +5,20 @@ import numpy as np
 #Neural Network
 class NeuralNetwork:
 
-    current_input = []
-    current_label =[]
-    #### ARCHITECTURE ##
-    no_of_input_features = None
-    # no of elements = no of hidden layers
-    # hidden_layer_sizes[i] = no of nodes in hidden layer 'i'
-    hidden_layer_sizes = []
-    output_classes = None
-
-    input_layer = None
-    W1 = None
-    h1 = None
-    W2 = None
-    output_layer = None
+    # current_input = []
+    # current_label =[]
+    # #### ARCHITECTURE ##
+    # no_of_input_features = None
+    # # no of elements = no of hidden layers
+    # # hidden_layer_sizes[i] = no of nodes in hidden layer 'i'
+    # hidden_layer_sizes = []
+    # output_classes = None
+    #
+    # input_layer = None
+    # W1 = None
+    # h1 = None
+    # W2 = None
+    # output_layer = None
 
     def __init__(self ,no_of_input_features, hidden_layer_sizes, output_classes):
 
@@ -50,12 +50,20 @@ class NeuralNetwork:
         self.input_layer.feed_values(self.current_input)
         self.h1.feed_values(self.current_input)
         self.h1.process_input()
-        hidden_layer_out_1 = self.h1.get_output()
+        hidden_layer_out_1 = self.h1.get_output_activated()
         self.output_layer.feed_values(hidden_layer_out_1)
         self.output_layer.process_input()
-        prob_dist = self.output_layer.get_output()
-        print("Probability Distribution"+str(prob_dist))
-        print("LOSS:" +str(np.square(self.current_label - prob_dist)))
+        prob_dist = self.output_layer.get_output_activated()
+        # print("Probability Distribution"+str(prob_dist))
+        # -log(prediction) label = 1
+        # -log(1-prediction) label =0
+        total_loss = 0
+        for i in range(self.output_classes):
+            if self.current_label == 0:
+                total_loss = total_loss + np.log(1-prob_dist)
+            elif self.current_label == 1:
+                total_loss = total_loss + np.log(prob_dist)
+        return -1*total_loss
 
     #Perform backpropagation
     def back_propagation_datapoint(self):
@@ -66,7 +74,12 @@ class NeuralNetwork:
         self.h1.compute_gradient()
         self.h1.update_weights()
 
-    def train(self, no_of_epochs):
+    def train(self, no_of_epochs, feed_dict = None):
+        self.no_of_training_data = len(feed_dict['label'])
+        total_cost = np.zeros(self.no_of_training_data)
         for i in range(no_of_epochs):
-            self.forward_feed_datapoint()
-            self.back_propagation_datapoint()
+            for j in range(self.no_of_training_data):
+                self.set_datapoint(feed_dict['input'][j], feed_dict['label'][j])
+                total_cost[j] =self.forward_feed_datapoint()
+                self.back_propagation_datapoint()
+            print("EPOCH "+ str(i) +" : \n Total Loss :" + str(np.sum(total_cost)))
